70ad7bb608880e1a9f4e72694b828af4ffdc8103
unknown <likhonsorkar02@yahoo.com>
2022-10-24 12:27:56 +0600

    swap variable value solution 1

---
+++ swap-variable.js
@@ -0,0 +1,12 @@
+//solution 1
+console.log('\n solution 1');
+var a = 70;
+var b = 100;
+// bfore swap 
+console.log("befor swap a =",a, "b =",b);
+// after swap
+var temp = a;
+a = b;
+b = temp;
+console.log("after swap a =",a, "b =",b);
+
