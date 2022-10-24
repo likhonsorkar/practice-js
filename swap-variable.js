3bc236008ffba0c3f913356a4fcbb5a7f8159077
unknown <likhonsorkar02@yahoo.com>
2022-10-24 12:29:48 +0600

    solution 2 add

--- swap-variable.js
+++ swap-variable.js
@@ -10,3 +10,13 @@ a = b;
 b = temp;
 console.log("after swap a =",a, "b =",b);
 
+// solution 2
+console.log('\n solution 2');
+
+var x = 150;
+var y = 75;
+// bfore swap 
+console.log("befor swap x =",x, "y =",y);
+// after swap 
+[x,y] = [y,x];
+console.log("after swap x =",x, "y =",y);
